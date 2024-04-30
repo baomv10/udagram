df91a3c887c3807c98d352e86104def539c48a0c
You <you@example.com>
2024-04-30 21:18:52 +0700

    init code

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=123456789
+export POSTGRES_HOST=database-1.cfoajq8ckrnu.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::baomvbucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=baomv
+export URL=http://localhost:8100
