4ab6c6dee56cba060c961a3cc1518a8ec2d4137f
You <you@example.com>
2024-05-01 21:04:29 +0700

    update code

--- udagram/udagram-frontend/src/app/api/api.service.ts
+++ udagram/udagram-frontend/src/app/api/api.service.ts
@@ -55,7 +55,8 @@ export class ApiService {
   }
 
   async upload(endpoint: string, file: File, payload: any): Promise<any> {
-    const signed_url = (await this.get(`${endpoint}/signed-url/${file.name}`)).url;
+    const url = `${API_HOST}${endpoint}`;
+    const signed_url = (await this.get(`${url}/signed-url/${file.name}`)).url;
 
     const headers = new HttpHeaders({'Content-Type': file.type});
     const req = new HttpRequest( 'PUT', signed_url, file,
