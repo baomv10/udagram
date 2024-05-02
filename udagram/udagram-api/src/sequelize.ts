7a84d93e956990e44d45b7619347f2185ea50cbf
You <you@example.com>
2024-05-02 18:34:34 +0700

    update code

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -4,6 +4,7 @@ export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
+  port: config.postgres_port,
   host: config.host,
   dialect: "postgres",
   storage: ":memory:",
